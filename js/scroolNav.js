27c1c9900149a035c10e28e2b134954803cf23a1
HunigoleSan <soy.hugo.pe@gmail.com>
2023-07-16 17:46:23 -0500

    agregando viñeta mobile y animacions en viñeta desktop

--- js/scroolNav.js
+++ js/scroolNav.js
@@ -1,13 +1,38 @@
-let vinetaHtml = document.getElementById("vineta")
-let catalogoHtml = document.getElementById("catalogo")
-function moverNavScroll(){
-    let scrollTop = document.documentElement.scrollTop
-    if(scrollTop  > 900){
-        vinetaHtml.classList.add("nav__bottom")
-        
-    }else{
-        vinetaHtml.classList.remove("nav__bottom")
+let vinetaHtml = document.getElementById("vineta");
+let catalogoHtml = document.getElementById("catalogo");
+let iconos_redes_sociales_HTML = document.querySelectorAll(".icon");
+console.log(iconos_redes_sociales_HTML.item);
+
+function moverNavScroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  console.log(document.documentElement.scrollHeight);
+  if (scrollTop > 900) {
+    vinetaHtml.classList.add("nav__bottom");
+    for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
+      setTimeout(function () {
+        iconos_redes_sociales_HTML[i].classList.add("icon_transform_desktop");
+      }, 500);
     }
-    
+  } else {
+    vinetaHtml.classList.remove("nav__bottom");
+    for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
+      setTimeout(function () {
+        iconos_redes_sociales_HTML[i].classList.remove(
+          "icon_transform_desktop"
+        );
+      }, 500);
+    }
+  }
 }
-window.addEventListener('scroll',moverNavScroll)
+/* let anchoPantala = 0;
+function vinetaMobileDisplay(){
+    if(anchoPantala <= 414){
+
+    }
+}
+window.addEventListener("resize", function () {
+  anchoPantala = window.innerWidth;
+  console.log(anchoPantala);
+});
+window.addEventListener("scroll", moverNavScroll);
+ */
