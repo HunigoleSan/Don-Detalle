58e0ca19f2cc99ad5b8c646c9ae85664779f02b7
HunigoleSan <soy.hugo.pe@gmail.com>
2024-06-22 01:38:49 -0500

    Limpiando Consola

--- js/mostrarProducto.js
+++ js/mostrarProducto.js
@@ -14,55 +14,45 @@ mostrarProductoCategoria()
 function mostrarProductoCategoria() {
     floresEternasHtml.style.display = "none";
     categoriaHtml.forEach(function (currentValue, index, array) {
-        /* console.log(currentValue) */
         currentValue.addEventListener('click', function () {
             categoria = currentValue.classList[1]
-            /* console.log(categoria) */
             if (categoria === 'bisuteria') {
-                /* console.log("bisuteria") */
                 bisuteriaHtml.style.display = 'grid';
                 origamiHtml.style.display = "none";
                 arregloHtml.style.display = "none";
                 llaveroHtml.style.display = "none";
                 floresEternasHtml.style.display = "none";
             } else if (categoria === 'origami') {
-                /* console.log("origami") */
+                
                 bisuteriaHtml.style.display = "none";
                 origamiHtml.style.display = 'grid';
                 arregloHtml.style.display = "none";
                 llaveroHtml.style.display = "none";
                 floresEternasHtml.style.display = "none";
             } else if (categoria === 'arreglo') {
-                console.log("arreglo")
                 arregloHtml.style.display = "grid";
                 origamiHtml.style.display = "none";
                 bisuteriaHtml.style.display = "none";
                 llaveroHtml.style.display = "none";
                 floresEternasHtml.style.display = "none";
             } else if (categoria === 'llavero') {
-                console.log("Llavero")
                 llaveroHtml.style.display = "grid";
                 arregloHtml.style.display = "none";
                 origamiHtml.style.display = "none";
                 bisuteriaHtml.style.display = "none";
                 floresEternasHtml.style.display = "none";
-
             } else if (categoria === 'floresEternas') {
-                console.log("Flores Eternas")
                 floresEternasHtml.style.display = "grid";
                 llaveroHtml.style.display = "none";
                 arregloHtml.style.display = "none";
                 origamiHtml.style.display = "none";
                 bisuteriaHtml.style.display = "none";
-                
-                
             }  else {
                 bisuteriaHtml.style.display = 'grid';
                 origamiHtml.style.display = 'grid';
                 arregloHtml.style.display = "grid";
                 llaveroHtml.style.display = "grid";
                 floresEternasHtml.style.display = "none";
-                /* console.log("Imprimir todos los productos") */
             }
 
         });
@@ -78,9 +68,7 @@ function imprimirProductos() {
     let llavero = 'llavero';
     let floresEternas = 'floresEternas';
     let listaCategoria = []
-    
-
-     for (let i = 0; i < objProductos.length; i++) {
+    for (let i = 0; i < objProductos.length; i++) {
         listaCategoria = objProductos[i][bisuteria]
         if(objProductos[i].hasOwnProperty(bisuteria)) {
             for (let producto in listaCategoria) {
@@ -101,7 +89,7 @@ function imprimirProductos() {
         }
     } 
     
-     for (let i = 0; i < objProductos.length; i++) {
+    for (let i = 0; i < objProductos.length; i++) {
         listaCategoria = objProductos[i][origami]
         if(objProductos[i].hasOwnProperty(origami)) {
             for (let producto in listaCategoria) {
@@ -161,11 +149,9 @@ function imprimirProductos() {
     }
     for (let i = 0; i < objProductos.length; i++) {
         listaCategoria = objProductos[i][floresEternas]
-        console.log(listaCategoria)
         if(objProductos[i].hasOwnProperty(floresEternas)) {
             let cartaJS = `<h2>${listaCategoria.proximamente}</h2>`
                 floresEternasHtml.innerHTML += cartaJS;
-                console.log(cartaJS)
         }
         
     }
--- js/scroolNav.js
+++ js/scroolNav.js
@@ -2,35 +2,38 @@ let vinetaHtml = document.getElementById("vineta");
 let vineta_mobile_HTML = document.getElementById("vineta_mobile");
 let catalogoHtml = document.getElementById("catalogo");
 let iconos_redes_sociales_HTML = document.querySelectorAll(".icon");
-console.log(iconos_redes_sociales_HTML.item);
 
+let statusNav = false
 function moverNavScroll() {
   let scrollTop = document.documentElement.scrollTop;
-  
-  if (scrollTop > 900) {
-    vinetaHtml.classList.add("nav__bottom");
-    for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
-      setTimeout(function () {
-        iconos_redes_sociales_HTML[i].classList.add("icon_transform_desktop");
-      }, 500);
+  if (statusNav == false) {
+    if (scrollTop > 900) {
+      vinetaHtml.classList.add("nav__bottom");
+      for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
+        setTimeout(function () {
+          iconos_redes_sociales_HTML[i].classList.add("icon_transform_desktop");
+        }, 500);
+      }
+      vineta_mobile_HTML.classList.remove("nav__bottom_mobile_top");
+      vineta_mobile_HTML.classList.add("nav__bottom_mobile");
+      statusNav = true
+    } else if(scrollTop < 900){
+
+      vinetaHtml.classList.remove("nav__bottom");
+      for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
+        setTimeout(function () {
+          iconos_redes_sociales_HTML[i].classList.remove("icon_transform_desktop");
+        }, 500);
+      }
+      statusNav = false
+      vineta_mobile_HTML.classList.remove("nav__bottom_mobile");
+      vineta_mobile_HTML.classList.add("nav__bottom_mobile_top");
+    
     }
-  } else {
-    vinetaHtml.classList.remove("nav__bottom");
-    for (let i = 0; i < iconos_redes_sociales_HTML.length; i++) {
-      setTimeout(function () {
-        iconos_redes_sociales_HTML[i].classList.remove("icon_transform_desktop");
-      }, 500);
-    }
+  }else{
+    statusNav = false
   }
 
-  if (scrollTop > 900) {
-    vineta_mobile_HTML.classList.remove("nav__bottom_mobile_top");
-    vineta_mobile_HTML.classList.add("nav__bottom_mobile");
-    console.log(vineta_mobile_HTML)
-  } else {
-    vineta_mobile_HTML.classList.remove("nav__bottom_mobile");
-    vineta_mobile_HTML.classList.add("nav__bottom_mobile_top");
-  }
 
 }
 
